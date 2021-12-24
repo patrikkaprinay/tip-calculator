1f6306ada599537d96780511591b8ebfa227d24e
Patrik Kaprinay <patrik.kaprinay@gmail.com>
2021-12-24 13:09:25 +0100

    finished work

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Logo from './components/Logo'
 function App() {
   return (
     <div className="w-screen h-screen bg-grayish-cyan-middle font-space-mono">
-      <div className="w-full h-5/6 flex flex-col items-center justify-center gap-24">
+      <div className="w-full h-full md:h-5/6 flex flex-col items-center justify-center gap-10 md:gap-24 p-3 lg:p-0">
         <Logo />
         <Calculator />
       </div>
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Calculator = () => {
   const [error, setError] = useState('')
@@ -14,39 +14,73 @@ const Calculator = () => {
       setPeople(0)
     } else {
       setError('')
-      setPeople(parseFloat(text))
+      if (isNaN(parseFloat(text))) {
+        setPeople(0)
+      } else {
+        setPeople(parseFloat(text))
+      }
     }
-    calculate()
   }
 
-  const changeBill = (e, text) => {
-    console.log(text)
-    text = parseFloat(text)
-    if (text === '0' || text === '' || isNaN(text)) {
-      setAmount(0)
-    } else {
-      setAmount(text)
-    }
-    calculate()
+  const changeBill = (text) => {
+    setAmount(text)
   }
 
-  const calculate = () => {
-    console.log('calculating')
+  const toTwoDecimals = (number) => {
+    return number.toFixed(2)
+  }
+
+  useEffect(() => {
+    const calculate = () => {
+      if (
+        amount !== '' &&
+        tip !== '' &&
+        people !== '' &&
+        amount !== 0 &&
+        typeof amount !== String &&
+        people !== 0 &&
+        !isNaN(tip)
+      ) {
+        let newTipperson = (parseFloat(amount) * (tip / 100)) / people
+        let newTotalPerson = (parseFloat(amount) * (tip / 100 + 1)) / people
+
+        setTipperson(toTwoDecimals(newTipperson))
+        setTotal(toTwoDecimals(newTotalPerson))
+      }
+    }
+    calculate()
+  }, [amount, tip, people])
+
+  const resetCalculator = () => {
+    setAmount('')
+    setTip(0)
+    setPeople('')
+    document.querySelector('#tipAmount').value = ''
+    setTipperson('0.00')
+    setTotal('0.00')
   }
 
   return (
-    <div className="bg-white w-full md:w-[900px] rounded-2xl px-8 py-6">
+    <div className="bg-white w-full lg:w-[870px] rounded-2xl p-12 min-w-[396px]">
       <div className="flex items-center flex-col md:flex-row gap-12 h-full">
-        <div className="flex items-center justify-center flex-col w-1/2">
-          <div className="w-full flex flex-col gap-6">
+        <div className="flex items-center justify-center flex-col w:full md:w-1/2">
+          <div className="w-full flex flex-col gap-6 font-bold">
             <div className="flex flex-col text-grayish-cyan-darkest gap-2 dollarBefore text-sm">
               <p>Bill</p>
               <input
-                onChange={(e) => changeBill(e, e.target.value)}
+                type="text"
+                onChange={(e) => changeBill(e.target.value)}
+                value={amount}
+                autoFocus
+                className="w-full bg-grayish-cyan-middle/50 focus:outline-strong-cyan
+                  outline-none px-3 py-1.5 rounded-md text-right relative text-dark-cyan/80 font-bold text-xl outline-offset-0"
+              />
+              {/* <input
+                onChange={(e) => changeBill(e.target.value)}
                 value={amount}
                 type="number"
                 className="bg-grayish-cyan-middle/50 focus:outline-strong-cyan outline-none px-3 py-1.5 rounded-md text-right relative text-dark-cyan/80 font-bold text-xl outline-offset-0"
-              />
+              /> */}
             </div>
             <div className="flex flex-col gap-2">
               <p className="text-grayish-cyan-darkest text-sm">Select Tip %</p>
@@ -93,8 +127,11 @@ const Calculator = () => {
                 </div>
                 <input
                   type="text"
+                  id="tipAmount"
+                  onClick={(e) => setTip(parseInt(e.target.value))}
                   onChange={(e) => setTip(parseInt(e.target.value))}
-                  className="bg-grayish-cyan-middle/50 p-2 text-xl rounded-md transition duration-150 text-dark-cyan font-bold text-right focus:outline-strong-cyan outline-none"
+                  placeholder="Custom"
+                  className="bg-grayish-cyan-middle/50 p-2 text-xl rounded-md transition duration-150 text-dark-cyan font-bold text-right focus:outline-strong-cyan outline-none outline-offset-0"
                 />
               </div>
             </div>
@@ -116,9 +153,9 @@ const Calculator = () => {
             </div>
           </div>
         </div>
-        <div className="flex items-center justify-center flex-col w-1/2 h-full">
+        <div className="flex items-center justify-center flex-col w-full md:w-1/2 h-full">
           <div className="w-full h-full bg-dark-cyan text-white rounded-lg py-5 px-9 flex flex-col justify-between">
-            <div className="flex flex-col gap-7 mt-5">
+            <div className="flex flex-col gap-7 my-5">
               <div className="flex justify-between items-center">
                 <div>
                   <p className="font-bold">Tip Amount</p>
@@ -145,7 +182,14 @@ const Calculator = () => {
               </div>
             </div>
             <div>
-              <button className="bg-grayish-cyan-middle w-full py-2 text-dark-cyan rounded-md uppercase font-bold hover:bg-grayish-cyan-middle/90 transition duration-150">
+              <button
+                onClick={() => resetCalculator()}
+                className={`${
+                  tip === 0 && amount === '' && people === ''
+                    ? 'opacity-30'
+                    : 'hover:bg-grayish-cyan-middle !cursor-pointer'
+                } bg-strong-cyan w-full py-2 text-dark-cyan rounded-md uppercase font-bold transition duration-150 cursor-default`}
+              >
                 Reset
               </button>
             </div>
