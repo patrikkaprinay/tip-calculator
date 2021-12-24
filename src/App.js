dfc1b1894a181e8cdb3a1d98246a7a17a46ab62c
Patrik Kaprinay <patrik.kaprinay@gmail.com>
2021-12-24 13:15:07 +0100

    fixed title

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Calculator from './components/Calculator'
 import Logo from './components/Logo'
 
 function App() {
+  document.title = 'Tip Calculator'
   return (
     <div className="w-screen h-screen bg-grayish-cyan-middle font-space-mono">
       <div className="w-full h-full md:h-5/6 flex flex-col items-center justify-center gap-10 md:gap-24 p-3 lg:p-0">
